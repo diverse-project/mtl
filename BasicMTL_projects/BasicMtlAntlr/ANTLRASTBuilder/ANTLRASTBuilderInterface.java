61fa810e21c4119f21e032e489e02f69a3eed725
jpthibau <jpthibau@da3e8b17-bafa-471d-a888-b3674b21d29d>
2003-07-10 07:48:03 +0000

    Initial revision needs to be tested on samples needs some more comments needs to split the .g file in 3 (for each class)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@2 da3e8b17-bafa-471d-a888-b3674b21d29d

---
+++ BasicMTL_projects/BasicMtlAntlr/ANTLRASTBuilder/ANTLRASTBuilderInterface.java
@@ -0,0 +1,85 @@
+package ANTLRASTBuilder;
+
+public interface ANTLRASTBuilderInterface {
+
+public Object library(Object header,java.util.Vector models,java.util.Vector methods,java.util.Vector classes);
+
+public Object libraryHeader(String lineNumber,String libName,Object inheritance,java.util.Vector tags);
+
+public Object nativeLibHeader(String lineNumber,String libName,java.util.Vector tags);
+
+public Object model(String lineNumber,String modelName,String viewName);
+
+public Object classDefinition(String lineNumber,String className,Object inheritance,java.util.Vector tags,java.util.Vector attributes,java.util.Vector methods);
+
+public Object inheritance (Object typesList);
+
+public Object attribute(Object locaVarDef,java.util.Vector tags);
+
+public Object method(String creation,String methodName,String lineNumber,Object parameters,Object returnedType,String throwsException,java.util.Vector localVars,java.util.Vector instructions,java.util.Vector tags);
+
+public Object varsDeclaration(Object typedVars,String lineNumber);
+
+public Object typedVars(java.util.Vector vars,Object type);
+
+public Object expressionInstr(Object expression,String lineNumber);
+
+public Object varSettingInstr(String varName,Object expression,String lineNumber);
+
+public Object returnInstr(Object expression,String lineNumber);
+
+public Object whileInstr(Object expression,java.util.Vector instructions,String lineNumber);
+
+public Object ifInstr(Object expression,java.util.Vector thenInstructions,java.util.Vector elseInstructions,String lineNumber);
+
+public Object throwsInstr(Object expression,String lineNumber);
+
+public Object tryInstr(java.util.Vector instructions,java.util.Vector catches,java.util.Vector finallyInstructions,String lineNumber);
+
+public Object associateInstr(java.util.Vector endPoints,String lineNumber);
+
+public Object associateEndPoint(String role,String endObjectName,Object type);
+
+public Object newExpr(Object theClass,String methodName,Object arguments,String lineNumber);
+
+public Object intLiteral(String value);
+
+public Object realLiteral(String value);
+
+public Object stringLiteral(String value);
+
+public Object selfLiteral();
+
+public Object nullLiteral();
+
+public Object trueLiteral();
+
+public Object falseLiteral();
+
+public Object variableName(String value);
+
+public Object expression(Object expression,java.util.Vector calledOperations);
+
+public Object operationCall(String operationName,Object arguments,String lineNumber);
+
+public Object oclAsType(Object type,String lineNumber);
+
+public Object arguments(java.util.Vector expressions);
+
+public Object typeName(java.util.Vector nameQualifiers);
+
+public Object tag(String lineNumber,String tagName,Object taggedValue);
+
+public Object boolTagValueTrue();
+
+public Object boolTagValueFalse();
+
+public Object intTagValue(String value);
+
+public Object realTagValue(String value);
+
+public Object stringTagValue(String value);
+
+public Object specialTagValue(String value);
+
+}
---
+++ BasicMTL_projects/BasicMtlAntlr/ANTLRASTBuilder/DummyBuilder.java
@@ -0,0 +1,85 @@
+package ANTLRASTBuilder;
+
+public class DummyBuilder implements ANTLRASTBuilderInterface {
+
+	public Object library(Object header,java.util.Vector models,java.util.Vector methods,java.util.Vector classes) { return null; }
+
+	public Object libraryHeader(String lineNumber,String libName,Object inheritance,java.util.Vector tags) { return null; }
+
+	public Object nativeLibHeader(String lineNumber,String libName,java.util.Vector tags) { return null; }
+
+	public Object model(String lineNumber,String modelName,String viewName) { return null; }
+
+	public Object classDefinition(String lineNumber,String className,Object inheritance,java.util.Vector tags,java.util.Vector attributes,java.util.Vector methods) { return null; }
+
+	public Object inheritance (Object typesList) { return null; }
+
+	public Object attribute(Object locaVarDef,java.util.Vector tags) { return null; }
+
+	public Object method(String creation,String methodName,String lineNumber,Object parameters,Object returnedType,String throwsException,java.util.Vector localVars,java.util.Vector instructions,java.util.Vector tags) { return null; }
+
+	public Object varsDeclaration(Object typedVars,String lineNumber) { return null; }
+
+	public Object typedVars(java.util.Vector vars,Object type) { return null; }
+
+	public Object expressionInstr(Object expression,String lineNumber) { return null; }
+
+	public Object varSettingInstr(String varName,Object expression,String lineNumber) { return null; }
+
+	public Object returnInstr(Object expression,String lineNumber) { return null; }
+
+	public Object whileInstr(Object expression,java.util.Vector instructions,String lineNumber) { return null; }
+
+	public Object ifInstr(Object expression,java.util.Vector thenInstructions,java.util.Vector elseInstructions,String lineNumber) { return null; }
+
+	public Object throwsInstr(Object expression,String lineNumber) { return null; }
+
+	public Object tryInstr(java.util.Vector instructions,java.util.Vector catches,java.util.Vector finallyInstructions,String lineNumber) { return null; }
+
+	public Object associateInstr(java.util.Vector endPoints,String lineNumber) { return null; }
+
+	public Object associateEndPoint(String role,String endObjectName,Object type) { return null; }
+
+	public Object newExpr(Object theClass,String methodName,Object arguments,String lineNumber) { return null; }
+
+	public Object intLiteral(String value) { return null; }
+
+	public Object realLiteral(String value) { return null; }
+
+	public Object stringLiteral(String value) { return null; }
+
+	public Object selfLiteral() { return null; }
+
+	public Object nullLiteral() { return null; }
+
+	public Object trueLiteral() { return null; }
+
+	public Object falseLiteral() { return null; }
+
+	public Object variableName(String value) { return null; }
+
+	public Object expression(Object expression,java.util.Vector calledOperations) { return null; }
+
+	public Object operationCall(String operationName,Object arguments,String lineNumber) { return null; }
+
+	public Object oclAsType(Object type,String lineNumber) { return null; }
+
+	public Object arguments(java.util.Vector expressions) { return null; }
+
+	public Object typeName(java.util.Vector nameQualifiers) { return null; }
+
+	public Object tag(String lineNumber,String tagName,Object taggedValue) { return null; }
+
+	public Object boolTagValueTrue() { return null; }
+
+	public Object boolTagValueFalse() { return null; }
+
+	public Object intTagValue(String value) { return null; }
+
+	public Object realTagValue(String value) { return null; }
+
+	public Object stringTagValue(String value) { return null; }
+
+	public Object specialTagValue(String value) { return null; }
+
+}
