d1d8b80d9cc3ec5ef4258831377ee922b4b3e04b
ffondeme <ffondeme@da3e8b17-bafa-471d-a888-b3674b21d29d>
2003-08-11 09:10:34 +0000

    A MetaClass is a type.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@70 da3e8b17-bafa-471d-a888-b3674b21d29d

--- RepositoryDriver_projects/RepositoryAPI/srcOfp/org/irisa/triskell/MT/repository/API/Java/MetaClass.java
+++ RepositoryDriver_projects/RepositoryAPI/srcOfp/org/irisa/triskell/MT/repository/API/Java/MetaClass.java
@@ -3,17 +3,17 @@ package org.irisa.triskell.MT.repository.API.Java;
 import java.lang.Exception;
 
 public interface MetaClass 
-    extends org.irisa.triskell.MT.repository.API.Java.MetaElement
+    extends org.irisa.triskell.MT.repository.API.Java.MetaElement, org.irisa.triskell.MT.DataTypes.Java.Type
 {
 
      String[] getQualifiedName();
 
      org.irisa.triskell.MT.repository.API.Java.ModelElement getMetaObject();
 
-     org.irisa.triskell.MT.repository.API.Java.ModelElementIterator allInstances()
+     org.irisa.triskell.MT.DataTypes.Java.CollectionValue allInstances()
         throws org.irisa.triskell.MT.repository.API.Java.UnknownElementException;
 
-     org.irisa.triskell.MT.repository.API.Java.ModelElementIterator allInstancesWithConstraint(
+     org.irisa.triskell.MT.DataTypes.Java.CollectionValue allInstancesWithConstraint(
         org.irisa.triskell.MT.repository.API.Java.LookupConstraint constraint)
         throws org.irisa.triskell.MT.repository.API.Java.UnknownElementException;
 
@@ -21,4 +21,8 @@ public interface MetaClass
         org.irisa.triskell.MT.repository.API.Java.ModelElement contextualElement,
         org.irisa.triskell.MT.DataTypes.Java.Value[] arguments)
         throws org.irisa.triskell.MT.repository.API.Java.UnknownElementException, org.irisa.triskell.MT.repository.API.Java.CommonException, org.irisa.triskell.MT.repository.API.Java.IllegalAccessException;
+
+     org.irisa.triskell.MT.repository.API.Java.ModelElementIterator allInstancesIterator(
+        org.irisa.triskell.MT.repository.API.Java.LookupConstraint constraint)
+        throws org.irisa.triskell.MT.repository.API.Java.UnknownElementException;
 }
