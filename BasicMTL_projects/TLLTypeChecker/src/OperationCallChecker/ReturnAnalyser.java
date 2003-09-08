656c7c571fe439c5ea05941132c6cd5273cef036
ffondeme <ffondeme@da3e8b17-bafa-471d-a888-b3674b21d29d>
2003-09-08 08:59:17 +0000

    "To invoke" computing : expressions of return and throws was forgotten
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@189 da3e8b17-bafa-471d-a888-b3674b21d29d

---
+++ BasicMTL_projects/TLLTypeChecker/src/OperationCallChecker/ReturnAnalyser.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 7 sept. 2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package OperationCallChecker;
+
+/**
+ * @author ffondeme
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class ReturnAnalyser extends TLLTopDownVisitor.ReturnAnalyser {
+
+
+}
