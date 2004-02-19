adb6e5ec34b52a841a693e7a335d68b65a2deaba
Didier Vojtisek <didier.vojtisek@inria.fr>
2004-02-19 10:02:54 +0000

    call CompilerTest main build file so any developper can easily add his own projects in the non regression tests procedure. Correction of the remove old build when diffusing the new distribution on the web site.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@452 da3e8b17-bafa-471d-a888-b3674b21d29d

--- BasicMTL_projects/BasicMtl2Java/autocompil/modelware_night_build.sh
+++ BasicMTL_projects/BasicMtl2Java/autocompil/modelware_night_build.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/tcsh
-# $Id: modelware_night_build.sh,v 1.4 2003-12-18 15:26:26 dvojtise Exp $
+# $Id: modelware_night_build.sh,v 1.5 2004-02-19 10:02:54 dvojtise Exp $
 # this script is run every night in order to verify that the latest files in the repository correctly compile
 # it runs some tests on the compiler in order to assure non regression.
 # sends email in case of trouble
@@ -48,7 +48,7 @@ if ( "$ERRORS_IN_BUILD" != "" ) then
     exit
 endif
 
-cd ../CompilerTests/src
+cd ../CompilerTests
 ant "build all" "run all" |& tee $BASE/ant_CompilerTests.log
 # check for errors and send mail to admin
 setenv ERRORS_IN_BUILD `grep FAILED $BASE/ant_CompilerTests.log`
@@ -61,17 +61,5 @@ if ( "$ERRORS_IN_BUILD" != "" ) then
     exit
 endif
 
-cd ../../UML1.4
-ant |& tee $BASE/ant_UML14.log
-# check for errors and send mail to admin
-setenv ERRORS_IN_BUILD `grep FAILED $BASE/ant_UML14.log`
-if ( "$ERRORS_IN_BUILD" != "" ) then
-	echo "The UML14 tests failed, please have a look in the log file: $BASE/ant_UML14.log " > msg.txt
-	echo "however the compiler compilation and CompilerTests were successfull"	>> msg.txt
-   	echo "---" >> msg.txt
-	cat $BASE/ant_UML14.log >> msg.txt
-   	cat msg.txt | Mail -s "[Modelware] Night build tests failed" modelware-cvs@irisa.fr
-    exit
-endif
-rm /site/w3e/WWW/modelware/htdocs/MTengine_latest_build/*.zip
+\rm /site/w3e/WWW/modelware/htdocs/MTengine_latest_build/*.zip
 cp $BASE/BasicMtl2Java/LibAssociation/dist/*.zip /site/w3e/WWW/modelware/htdocs/MTengine_latest_build/
