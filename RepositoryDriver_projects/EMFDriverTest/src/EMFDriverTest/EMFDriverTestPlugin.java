f064eb32cd10f08c3a3d70db14681341032fe33b
jpthibau <jpthibau@da3e8b17-bafa-471d-a888-b3674b21d29d>
2004-03-08 08:09:32 +0000

    EMFDriver first Tests : model loading,save and creation; allInstances attributes access for attributes of "primitive types"  types
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@465 da3e8b17-bafa-471d-a888-b3674b21d29d

---
+++ RepositoryDriver_projects/EMFDriverTest/src/EMFDriverTest/EMFDriverTestPlugin.java
@@ -0,0 +1,17 @@
+package EMFDriverTest;
+
+import org.eclipse.ui.plugin.*;
+import org.eclipse.core.runtime.*;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class EMFDriverTestPlugin extends AbstractUIPlugin {
+	
+	/**
+	 * The constructor.
+	 */
+	public EMFDriverTestPlugin(IPluginDescriptor descriptor) {
+		super(descriptor);
+	}
+}
