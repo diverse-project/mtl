030d22d810fb22fdf5ac47abf3338c3a4d86d13c
edrezen <edrezen@da3e8b17-bafa-471d-a888-b3674b21d29d>
2004-04-21 18:11:46 +0000

    Initial version
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@539 da3e8b17-bafa-471d-a888-b3674b21d29d

---
+++ BasicMTL_projects/BasicMtlAstJava/srcOfp/org/irisa/triskell/MT/BasicMTL/BasicMTLAST/Java/Foreach.java
@@ -0,0 +1,67 @@
+package org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java;
+
+import java.util.*;
+import org.irisa.triskell.MT.visitors.Java.GenericVisitor.*;
+import org.irisa.triskell.MT.visitors.Java.AnalysingVisitor.*;
+import org.irisa.triskell.MT.visitors.Java.GenericVisitor.Visitor;
+import org.irisa.triskell.MT.visitors.Java.AnalysingVisitor.Property;
+import java.util.Map;
+import java.lang.Object;
+
+public class Foreach 
+    extends org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Instruction
+{
+    protected Vector body = new Vector();
+    public org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Instruction getBody (int i) {
+        return (org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Instruction)this.body.elementAt(i);
+    }
+    public void setBody (int i, org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Instruction value) {
+        this.body.setElementAt(value, i);
+    }
+    public void appendBody (org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Instruction value) {
+        this.body.addElement(value);
+    }
+    public void eraseBody (org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Instruction value)  {
+        this.body.removeElement(value);
+    }
+    public void eraseBody (int i)  {
+        this.body.removeElementAt(i);
+    }
+    public int cardBody () {
+        return this.body.size();
+    }
+
+    protected org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.VarDeclaration varDeclaration;
+    public org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.VarDeclaration getVarDeclaration () {
+        return this.varDeclaration;
+    }
+    public void setVarDeclaration (org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.VarDeclaration value) {
+        this.varDeclaration = value; 
+    }
+    public int cardVarDeclaration () {
+        if ( this.varDeclaration == null ) return 0;
+        else return 1;
+    }
+
+    protected org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Expression collection;
+    public org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Expression getCollection () {
+        return this.collection;
+    }
+    public void setCollection (org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Expression value) {
+        this.collection = value; 
+    }
+    public int cardCollection () {
+        if ( this.collection == null ) return 0;
+        else return 1;
+    }
+
+
+    public Foreach(
+        org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.VarDeclaration varDeclaration,
+        org.irisa.triskell.MT.BasicMTL.BasicMTLAST.Java.Expression collection)
+    {
+	setVarDeclaration (varDeclaration);
+    	setCollection (collection);
+
+    }
+}
---
+++ BasicMTL_projects/BasicMtlTLLJava/src/TLLTopDownVisitor/ForeachAnalyser.java
@@ -0,0 +1,92 @@
+/*
+ * $Header: /tmp/cvs2svn/cvsroot/BasicMTL_projects/BasicMtlTLLJava/src/TLLTopDownVisitor/ForeachAnalyser.java,v 1.1 2004-04-21 18:07:08 edrezen Exp $
+ * Created on 24 juil. 2003
+ *
+ */
+package TLLTopDownVisitor;
+
+import org.irisa.triskell.MT.visitors.Java.AnalysingVisitor.*;
+import org.irisa.triskell.MT.visitors.Java.GenericVisitor.*;
+import org.irisa.triskell.MT.BasicMTL.BasicMTLTLL.Java.*;
+
+/**
+ * @author jpthibau
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class ForeachAnalyser extends Analyser {
+
+	public ForeachAnalyser()
+	{	super(Foreach.class);
+	}
+
+	public void analyse (Visitable node,Visitor visitor,java.util.Map context)
+	{	
+		Foreach ASTnode = (Foreach) node;
+		Object theCreatedForeach = ForeachBefore (ASTnode,context);
+
+		ASTnode.getCollection().accept (visitor,context);
+		this.ForeachCollection (theCreatedForeach, context.get("Instruction"), context);
+
+		//ASTnode.getVarDeclaration().accept (visitor,context);
+		this.ForeachVarDeclaration (theCreatedForeach, context.get("VarDeclaration"), context);
+
+		// we loop over the instructions of the foreach body
+		for (int i=0; i<ASTnode.cardBody(); i++) 
+		{
+			((Instruction)ASTnode.getBody(i)).accept (visitor,context);
+			this.ForeachBodyInstruction (theCreatedForeach,context.get("Instruction"),context);
+		}
+
+		ForeachAfter (theCreatedForeach,ASTnode,context);
+	}
+
+	/** */
+	public Object ForeachBefore (
+		Foreach ASTnode,
+		java.util.Map context
+	)	
+	{	
+		return null; 
+	}
+
+	/** */
+	public void ForeachAfter (
+		Object theForeach, 
+		Foreach ASTnode,
+		java.util.Map context
+	) 
+	{
+	}
+
+
+	/** */
+	public void ForeachVarDeclaration (
+		Object theForeach, 
+		Object varDeclaration, 
+		java.util.Map context
+	) 
+	{
+	}
+
+
+	/** */
+	public void ForeachCollection (
+		Object theForeach, 
+		Object collection, 
+		java.util.Map context
+	) 
+	{
+	}
+
+
+	/** */
+	public void ForeachBodyInstruction (
+		Object theForeach, 
+		Object instr, 
+		java.util.Map context
+	) 
+	{
+	}
+}
