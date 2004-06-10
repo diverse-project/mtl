47ba361939cfec6359d78070927c2e3e2aec3286
jpthibau <jpthibau@da3e8b17-bafa-471d-a888-b3674b21d29d>
2004-06-10 10:19:27 +0000

    Centralized compiler errors manager
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@600 da3e8b17-bafa-471d-a888-b3674b21d29d

--- BasicMTL_projects/Utils/src/org/irisa/triskell/MT/utils/MessagesHandler/MSGHandler.java
+++ BasicMTL_projects/Utils/src/org/irisa/triskell/MT/utils/MessagesHandler/MSGHandler.java
@@ -1,4 +1,4 @@
-/* $Id: MSGHandler.java,v 1.2 2004-06-09 09:35:08 jpthibau Exp $
+/* $Id: MSGHandler.java,v 1.3 2004-06-10 10:19:26 jpthibau Exp $
  * Authors : 
  * 
  * Copyright 2003 - INRIA - LGPL license
@@ -30,31 +30,47 @@ public class MSGHandler {
 	}
 	
 	public static void init() {
+		allMessages=new Vector();
 		try {
 			String filePath = new java.io.File(Directories.getRootPath(MSGHandler.class.getName()) + "/log4j_configuration.xml").getCanonicalPath();
 			LogManager.resetConfiguration();
 			DOMConfigurator.configure(filePath); }
 		catch(java.io.IOException e) {
 							System.err.println("Can't state log4j in MSGHandler"); }
-		allMessages=new Vector();
 	}
 	
 	public static void info(Class origin,int originLineNumber,String msg) {
 		log.info(msg);
+		if (allMessages == null)
+		init();
 		allMessages.add(new CompilerMessage("info",msg,processedMtlFile,processedMtlLineNumber,origin,originLineNumber));		
 	}
 
 	public static void warn(Class origin,int originLineNumber,String msg) {
 		log.warn(msg);
+		if (allMessages == null)
+		init();
 		allMessages.add(new CompilerMessage("warn",msg,processedMtlFile,processedMtlLineNumber,origin,originLineNumber));		
 	}
 	public static void debug(Class origin,int originLineNumber,String msg) {
 		log.debug(msg);
+		if (allMessages == null)
+		init();
 		allMessages.add(new CompilerMessage("debug",msg,processedMtlFile,processedMtlLineNumber,origin,originLineNumber));		
 	}
 	
 	public static void error(Class origin,int originLineNumber,String msg) {
 		log.error(msg);
+		if (allMessages == null)
+		init();
 		allMessages.add(new CompilerMessage("error",msg,processedMtlFile,processedMtlLineNumber,origin,originLineNumber));		
+	}
+
+	public static void fatal(Class origin,int originLineNumber,String msg) throws Exception {
+		log.error(msg);
+		if (allMessages == null)
+		init();
+		allMessages.add(new CompilerMessage("fatal",msg,processedMtlFile,processedMtlLineNumber,origin,originLineNumber));
+		throw new Exception("Basic MTL Fatal Error.");
 	}
 }
