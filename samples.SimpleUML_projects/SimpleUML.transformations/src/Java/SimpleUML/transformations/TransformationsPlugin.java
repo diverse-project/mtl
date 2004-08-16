a2175ee72dd71d41a45ec08cdf9c4e7aa1f48b85
Didier Vojtisek <didier.vojtisek@inria.fr>
2004-08-16 11:46:38 +0000

    added plugin entry point class for loading bundle resources adapted the src folder to be able to deal with both MTL and java files
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@676 da3e8b17-bafa-471d-a888-b3674b21d29d

---
+++ samples.SimpleUML_projects/SimpleUML.transformations/src/Java/SimpleUML/transformations/TransformationsPlugin.java
@@ -0,0 +1,69 @@
+package SimpleUML.transformations;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+import java.util.*;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class TransformationsPlugin extends Plugin {
+	//The shared instance.
+	private static TransformationsPlugin plugin;
+	//Resource bundle.
+	private ResourceBundle resourceBundle;
+	
+	/**
+	 * The constructor.
+	 */
+	public TransformationsPlugin() {
+		super();
+		plugin = this;
+		try {
+			resourceBundle = ResourceBundle.getBundle("SimpleUML.transformations.TransformationsPluginResources");
+		} catch (MissingResourceException x) {
+			resourceBundle = null;
+		}
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static TransformationsPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the string from the plugin's resource bundle,
+	 * or 'key' if not found.
+	 */
+	public static String getResourceString(String key) {
+		ResourceBundle bundle = TransformationsPlugin.getDefault().getResourceBundle();
+		try {
+			return (bundle != null) ? bundle.getString(key) : key;
+		} catch (MissingResourceException e) {
+			return key;
+		}
+	}
+
+	/**
+	 * Returns the plugin's resource bundle,
+	 */
+	public ResourceBundle getResourceBundle() {
+		return resourceBundle;
+	}
+}
