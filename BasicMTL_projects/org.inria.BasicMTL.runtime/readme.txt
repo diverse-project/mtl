ed793594a5e7fff099982f2e898b7742336b7bf9
Didier Vojtisek <didier.vojtisek@inria.fr>
2004-08-16 11:40:19 +0000

    added plugin entry point class for loading bundle resources
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@675 da3e8b17-bafa-471d-a888-b3674b21d29d

--- BasicMTL_projects/org.inria.BasicMTL.runtime/readme.txt
+++ BasicMTL_projects/org.inria.BasicMTL.runtime/readme.txt
@@ -1,4 +1,4 @@
-$Id: readme.txt,v 1.1 2004-08-10 12:28:05 dvojtise Exp $
+$Id: readme.txt,v 1.2 2004-08-16 11:40:17 dvojtise Exp $
 
 This project aims at providing the BasicMTL runtime as an Eclipse plugin 
 so it may be used by other plugins
@@ -8,3 +8,8 @@ then it package it, jar file, + copy of used jars like MDR, log4J, etc
 
 note: EMF and EMFDriver isn't included here, as this is the role 
  of org.inria.EMFDriver to to it.
+ 
+ Note2: a lot of trouble finilizing this plugin, I still get a lot of classNotFound or undefClass exceptions
+ when using this plugin from another one.
+ This seems to be a configuration problem using the bundle loçading management
+ If someone as an idea how to fix that ...
---
+++ BasicMTL_projects/org.inria.BasicMTL.runtime/src/org/inria/BasicMTL/runtime/RuntimePlugin.java
@@ -0,0 +1,69 @@
+package org.inria.BasicMTL.runtime;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+import java.util.*;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class RuntimePlugin extends Plugin {
+	//The shared instance.
+	private static RuntimePlugin plugin;
+	//Resource bundle.
+	private ResourceBundle resourceBundle;
+	
+	/**
+	 * The constructor.
+	 */
+	public RuntimePlugin() {
+		super();
+		plugin = this;
+		try {
+			resourceBundle = ResourceBundle.getBundle("org.inria.BasicMTL.runtime.RuntimePluginResources");
+		} catch (MissingResourceException x) {
+			resourceBundle = null;
+		}
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static RuntimePlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the string from the plugin's resource bundle,
+	 * or 'key' if not found.
+	 */
+	public static String getResourceString(String key) {
+		ResourceBundle bundle = RuntimePlugin.getDefault().getResourceBundle();
+		try {
+			return (bundle != null) ? bundle.getString(key) : key;
+		} catch (MissingResourceException e) {
+			return key;
+		}
+	}
+
+	/**
+	 * Returns the plugin's resource bundle,
+	 */
+	public ResourceBundle getResourceBundle() {
+		return resourceBundle;
+	}
+}
