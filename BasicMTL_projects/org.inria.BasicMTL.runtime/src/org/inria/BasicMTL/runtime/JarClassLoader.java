caf7601fad3d059b782454f832422c0d21287bb7
Didier Vojtisek <didier.vojtisek@inria.fr>
2004-08-18 07:44:07 +0000

    Initial revision
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@678 da3e8b17-bafa-471d-a888-b3674b21d29d

---
+++ BasicMTL_projects/org.inria.BasicMTL.runtime/src/org/inria/BasicMTL/runtime/JarClassLoader.java
@@ -0,0 +1,63 @@
+/*
+ * $Id: JarClassLoader.java,v 1.1 2004-08-18 07:44:07 dvojtise Exp $
+ * Created on 16 août 2004
+ * Copyright (c) 2002 Object Factory Inc. All rights reserved.
+ * Permission to copy granted under the MIT license
+ * http://www.opensource.org/licenses/mit-license.php
+ * adapted from http://eclipsewiki.swiki.net/123
+ */
+
+package org.inria.BasicMTL.runtime;
+
+import java.net.URLClassLoader;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+/**
+ * Used to load MDR classes independent of any use of MDR by other plugins.
+ * @author dvojtise, Bob Foster
+ */
+public class JarClassLoader extends URLClassLoader {
+	public JarClassLoader(URL[] urls, ClassLoader parent) {
+		super(urls, parent);
+	}
+	
+	protected Class findClass(String name) throws ClassNotFoundException {
+		Class c = super.findClass(name);
+		return c;
+	}
+	
+	protected Class loadClass(String name, boolean resolve) throws ClassNotFoundException {
+		
+		// All non-MDR classes handled normally
+		System.out.println("  org.inria.BasicMTL.runtime.JarClassLoader trying to load: "+name);
+		if (!name.startsWith("org.netbeans.mdr")
+			/*	&& !name.startsWith("org.xml.sax.")
+				&& !name.startsWith("javax.xml.")*/) {
+			System.out.println("  org.inria.BasicMTL.runtime.JarClassLoader    looking in super()");
+			return super.loadClass(name, resolve);
+		}
+		
+		// Other versions of MDR classes may already be
+		// loaded. Make sure we don't get any of these by
+		// trying to load locally first.
+		
+		Class c = (Class) fMap.get(name);
+		
+		if (c == null) {
+			c = findClass(name);
+		
+			if (c != null)
+				fMap.put(name, c);
+			else {
+				c = super.loadClass(name);
+			}
+		}
+		
+		if (c != null && resolve)
+			resolveClass(c);
+			
+		return c;
+	}
+	HashMap fMap = new HashMap();
+}
