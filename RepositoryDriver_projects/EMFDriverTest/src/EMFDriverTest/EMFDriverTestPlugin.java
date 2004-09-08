11094d0eb45d96786e186a1cea6f69e8951733d7
jpthibau <jpthibau@da3e8b17-bafa-471d-a888-b3674b21d29d>
2004-09-08 12:28:42 +0000

    EMFDriverTest updated version with same types names as MDR driver use.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@712 da3e8b17-bafa-471d-a888-b3674b21d29d

--- RepositoryDriver_projects/EMFDriverTest/src/EMFDriverTest/EMFDriverTestPlugin.java
+++ RepositoryDriver_projects/EMFDriverTest/src/EMFDriverTest/EMFDriverTestPlugin.java
@@ -1,17 +1,69 @@
 package EMFDriverTest;
 
-import org.eclipse.ui.plugin.*;
-import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+import java.util.*;
 
 /**
  * The main plugin class to be used in the desktop.
  */
-public class EMFDriverTestPlugin extends AbstractUIPlugin {
+public class EMFDriverTestPlugin extends Plugin {
+	//The shared instance.
+	private static EMFDriverTestPlugin plugin;
+	//Resource bundle.
+	private ResourceBundle resourceBundle;
 	
 	/**
 	 * The constructor.
 	 */
-	public EMFDriverTestPlugin(IPluginDescriptor descriptor) {
-		super(descriptor);
+	public EMFDriverTestPlugin() {
+		super();
+		plugin = this;
+		try {
+			resourceBundle = ResourceBundle.getBundle("EMFDriverTest.EMFDriverTestPluginResources");
+		} catch (MissingResourceException x) {
+			resourceBundle = null;
+		}
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static EMFDriverTestPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the string from the plugin's resource bundle,
+	 * or 'key' if not found.
+	 */
+	public static String getResourceString(String key) {
+		ResourceBundle bundle = EMFDriverTestPlugin.getDefault().getResourceBundle();
+		try {
+			return (bundle != null) ? bundle.getString(key) : key;
+		} catch (MissingResourceException e) {
+			return key;
+		}
+	}
+
+	/**
+	 * Returns the plugin's resource bundle,
+	 */
+	public ResourceBundle getResourceBundle() {
+		return resourceBundle;
 	}
 }
