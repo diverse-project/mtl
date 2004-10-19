041203fef2a18d1baffa99120d83a72de4245cb5
Didier Vojtisek <didier.vojtisek@inria.fr>
2004-10-19 07:02:40 +0000

    better error handling when the method ClassLoader.getSystemRessource doesn't give the expected result. Sometimes, this occurs in Eclipse context.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@816 da3e8b17-bafa-471d-a888-b3674b21d29d

--- BasicMTL_projects/Utils/src/org/irisa/triskell/MT/utils/Java/Directories.java
+++ BasicMTL_projects/Utils/src/org/irisa/triskell/MT/utils/Java/Directories.java
@@ -1,4 +1,4 @@
-/* $Id: Directories.java,v 1.6 2004-07-01 10:24:28 edrezen Exp $
+/* $Id: Directories.java,v 1.7 2004-10-19 07:02:40 dvojtise Exp $
  * Created on May 22, 2003
  *
  * Authors : ffondemen
@@ -56,8 +56,18 @@ public class Directories {
     	String ret = System.getProperty("Directories.RootPath");
     	if (ret != null && ret.length() > 0)
     		return new File(ret).getAbsolutePath();
-        String thisClassName = ClassLoader.getSystemResource(AWK.replace(fromClass,
-                ".", "/") + ".class").getFile();
+    	java.net.URL theURL = ClassLoader.getSystemResource(AWK.replace(fromClass,
+                ".", "/") + ".class");
+    	
+    	if (theURL == null) 
+    	{ 	// unable to retreve the class in this context, 
+    		// may be trying the getRootPath that get a ClasLoader in its parameters ?
+    		System.err.println("Unable to retrieve RootPath form either the system property \"Directories.RootPath\" or from this resource: " + fromClass);
+    		System.err.println("Please set the system property \"Directories.RootPath\" or from this resource: " + fromClass);
+    		
+    		return null;	
+    	}
+        String thisClassName = theURL.getFile();
         if (thisClassName.startsWith("file:")) {
             int index = thisClassName.indexOf("!");
             if (index < 0)      // Should not happen, we are in a jar
