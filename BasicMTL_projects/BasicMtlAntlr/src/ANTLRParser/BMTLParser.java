5db692edc27b2532a48ae0b8cdc91892b190f3e7
edrezen <edrezen@da3e8b17-bafa-471d-a888-b3674b21d29d>
2004-11-04 14:07:55 +0000

    This version adds 3 things :
    1) The BMTLParser class is now an Observable and can notify Observers objects. In particular, a notification is done when an parsing error occurs.
    
    2) The BMTLParser class becomes a Singleton => static methods are no longer static. It was needed to be able for Observers objects to subscribe to a specific (here a single one) instance of the BMTLParser class.
    
    3) More precise exception handle : previously, only ANTLRException was handled and had poor information about the error. Now, children class of ANTLRException are caught which provide more infomation (like filename, line, column...)
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@915 da3e8b17-bafa-471d-a888-b3674b21d29d

--- BasicMTL_projects/BasicMtlAntlr/src/ANTLRParser/BMTLParser.java
+++ BasicMTL_projects/BasicMtlAntlr/src/ANTLRParser/BMTLParser.java
@@ -1,5 +1,5 @@
 /*
- * $Header: /tmp/cvs2svn/cvsroot/BasicMTL_projects/BasicMtlAntlr/src/ANTLRParser/BMTLParser.java,v 1.12 2004-10-18 16:00:04 jpthibau Exp $
+ * $Header: /tmp/cvs2svn/cvsroot/BasicMTL_projects/BasicMtlAntlr/src/ANTLRParser/BMTLParser.java,v 1.13 2004-11-04 14:07:55 edrezen Exp $
  * Created on 16 juil. 2003
  *
  */
@@ -17,37 +17,83 @@ import java.io.*;
 import org.apache.log4j.Logger;
 import org.irisa.triskell.MT.utils.MessagesHandler.MSGHandler;
 
+import antlr.RecognitionException;
+import antlr.TokenStreamRecognitionException;
+
+
 import ANTLRASTWalker.*;
 
 
-public class BMTLParser {
+public class BMTLParser extends java.util.Observable 
+{
+	// we define a singleton for this class
+	final static private BMTLParser singleton = new BMTLParser ();
+	static public BMTLParser instance() { return singleton; }
+	
 	static DataInputStream input;
 	static final Logger log=MSGHandler.init();
 
 
-	public static Object Parse(String name,ANTLRWalkerActionsInterface aWalker)
+	public Object Parse(String name,ANTLRWalkerActionsInterface aWalker)
 	{	log.debug("Parsing : "+name);
 		boolean noPb=true;
 		Object builtTree=null;
 		try { input=new DataInputStream(new FileInputStream(name)); }
 		catch (FileNotFoundException e) {
 			log.error("PB opening input file : "+name);}
+
 		try {
 			BasicmtlLexer lexer = new BasicmtlLexer(input);
 			BasicmtlParser parser = new BasicmtlParser(lexer);
 			builtTree=parser.basicMTL(lexer,aWalker);
-			}
+		}
+		
+		catch (RecognitionException e) {
+			noPb=false;
+			log.warn("RecognitionException on "+name+", "+e);
+			log.warn("=> "+e.getMessage());
+			e.printStackTrace();
+
+			// we send a message to potential observers
+			this.notifyObservers (
+				"RecognitionException",
+				e.getMessage(), 
+				(e.getFilename()!=null ? e.getFilename() : name),
+				new Integer(e.getLine()),
+				new Integer(e.getColumn())
+			);
+		}
+
+		catch (TokenStreamRecognitionException e) {
+			noPb=false;
+			log.warn("TokenStreamRecognitionException on "+name+", "+e);
+			log.warn("=> "+e.getMessage());
+			e.printStackTrace();			
+			
+			// we send a message to potential observers
+			this.notifyObservers (
+				"RecognitionException",
+				e.recog.getMessage(), 
+				(e.recog.getFilename()!=null ? e.recog.getFilename() : name),
+				new Integer(e.recog.getLine()),
+				new Integer(e.recog.getColumn())
+			);
+		}
+
 		catch (antlr.ANTLRException e) {
 			noPb=false;
 			log.warn("ANTLRException on "+name+", "+e);
 			log.warn("=> "+e.getMessage());
 			e.printStackTrace();
 		}
+		
 		catch(Exception e) {
 			noPb=false;
 			log.warn("exception: "+e+"=>"+e.getMessage());
 			log.warn("exception was raised when parsing : "+name+", ");
-			e.printStackTrace(); }				
+			e.printStackTrace(); 
+		}				
+		
 		log.debug("For the file "+name+',');
 		log.debug("parsing is over.");
 		if (noPb) return builtTree;
@@ -58,10 +104,26 @@ public class BMTLParser {
 	{	DummyWalker aWalker=new DummyWalker();
 		if (args.length > 0)
 			for (int i=0;i<args.length;i++)
-				try { Parse(args[i],aWalker); }
+				try { BMTLParser.instance().Parse(args[i],aWalker); }
 				catch (Exception e) {
 					log.error(e.getMessage());
 					e.printStackTrace();}
 		else log.error("USAGE : java BMTL <sourcefiles>");
 	}
+	
+	
+	/** */
+	private void notifyObservers (
+		String type,
+		String message,
+		String fileName,
+		Integer line,
+		Integer column
+	) 
+	{
+		setChanged();
+		super.notifyObservers (new Object[] {type, message, fileName, line, column});
+		clearChanged();
+	}
+
 }
