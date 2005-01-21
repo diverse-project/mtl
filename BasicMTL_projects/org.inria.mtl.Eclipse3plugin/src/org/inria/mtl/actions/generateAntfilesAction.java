ad4f6c18cb9014dc148cd3c7fdaffd3a4130006e
zdrey <zdrey@da3e8b17-bafa-471d-a888-b3674b21d29d>
2005-01-21 11:58:21 +0000

    - tidied the code - changed the way to access the directory of the .jars of MTL (inputDialog)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@973 da3e8b17-bafa-471d-a888-b3674b21d29d

--- BasicMTL_projects/org.inria.mtl.Eclipse3plugin/src/org/inria/mtl/actions/generateAntfilesAction.java
+++ BasicMTL_projects/org.inria.mtl.Eclipse3plugin/src/org/inria/mtl/actions/generateAntfilesAction.java
@@ -1,34 +1,37 @@
-/*
+ /*
  * Project  : org.inria.mtl.Eclipse3plugin
  * Filename : generateAntfilesAction.java
- * License  : GPL
+ * License  : LGPL
  * Author   : zdrey@irisa.fr
  * 
  * Creation date     : Jan 19, 2005
  * Modification date : Jan 19, 2005
  * 
- * Description       :
- * 
- * This mini-project is intended for didactic purpose
- * as an example of simple compiler/interpreter.
- * 
+ * Description       : 
+ * This code creates a Button that is associated to this action :
+ * the generation, for the currently selected MTL Project, of a set of *_build.xml files, from the informations
+ * given in .mtlclasspath and .classpath of the MTL Project, so that the user can then compile and run a MTL Project without
+ * bothering at writing manually systematic ant files, that
+ * he created with the Eclipse MTL plugin, outside of Eclipse.
  */
 package org.inria.mtl.actions;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
+import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -38,6 +41,7 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.inria.mtl.MTLPlugin;
 
 /**
  * @author zdrey
@@ -46,7 +50,8 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
  */
 public class generateAntfilesAction implements IWorkbenchWindowActionDelegate {
 
-	
+	private ResourceBundle resourceBundle;
+	private ResourcesPlugin resourcesplugin;
 	private IWorkbenchWindow window;
 	private StructuredSelection currentSelection = null;
 	private ISelection selection=null;
@@ -69,74 +74,125 @@ public class generateAntfilesAction implements IWorkbenchWindowActionDelegate {
 		this.window = window;
 	}
 
+	/**
+	  *  Gets the baseURL attribute of the MTLPlugin directory
+	  *  (copied from MTLPlugin main class), and add a path chunk to it.
+	  * @param a path chunk, that is a path relative to the current plugin directory
+	  * @return    The platform-dependent absolute path of the given "path chunk"
+	  */
 	
-	protected String getProjectPath()
+	public String getPathFromRelative(String pathChunk)
+	{		
+		String path = "";
+		URL cmd_url;
+		try
+		{			
+			cmd_url = new URL(
+					org.inria.mtl.MTLPlugin.getBaseURL(),
+					pathChunk);
+		    path = Platform.resolve(cmd_url).getPath();
+		}
+		catch (Exception e)
+		{
+			// should not append
+			System.err.print("Could not find "+ pathChunk +" file");
+			e.printStackTrace();
+		}
+		return path;
+	}
+	
+	public String getDefaultBasicMTLc()
 	{
-		//String str = ResourcesPlugin.getPlugin().
-		//File file = new File(myWorkspaceRoot.getProject("org.inria.mtl.Eclipse3plugin").getFullPath().toOSString());
-		
-//		System.out.println(str);
-//		URL url = new URL(getBaseURL(), iconPath + name);
-		System.out.println(new File("../../").getAbsolutePath());
-	//	return new File("../../").toString() ;
-		return "/udd/zdrey/Workspace/org.inria.mtl.Eclipse3plugin/python/src/org/irisa/antimtl/Antimtl.py";
+		System.setProperty ("Directories.RootPath",  
+				MTLPlugin.getDefault().getLocation() +	System.getProperty("file.separator")
+				+ "MTL" + System.getProperty("file.separator") + "bin");
+		return "";
 	}
 	
 	/**
-	 * Run the python Antimtl :
+	 * Run the python Antimtl.py on the selected project, or, if user specified either many or no
+	 * project(s), ask him to choose one through an inputDialog.
 	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
 	 */
-	public void run(IAction action) {
-		
-		String cmd = this.getProjectPath();
-		// Get the current directory?
-		String options = " --cp=.classpath --mtlcp=.mtlclasspath --output=.";
+	public void run(IAction action) 
+	{
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProjects()[0];
+		String project_path = "";
 		String line = ""; String str = ""; String errstr = "";
-		try
+		Shell shell = window.getShell();
+		
+		// A property that is set in MTLPlugin.java when log4j is not configured...(getLogger method) I need this property.
+		String rootpath = System.getProperty("Directories.RootPath");
+		String sep 		= System.getProperty("file.separator");
+		if (rootpath == null)
 		{
-			Process py_proc = Runtime.getRuntime().exec("python "+cmd+options);
-			Shell shell = window.getShell();
-		    BufferedReader error_stream = new BufferedReader(
-		    		new InputStreamReader(py_proc.getErrorStream()));
-		    BufferedReader input_stream = new BufferedReader(
-		    		new InputStreamReader(py_proc.getInputStream()));
-		    
-		    try {
-		    	while ((line = error_stream.readLine()) != null) 
-		    	{
-		    		errstr += line;
-		    	}
-		    }
-		    catch (IOException e)
-			{
-		    	System.exit(0);
-		    }
-		    
-		    try {
-		    	while ((line = input_stream.readLine()) != null) 
-		    	{
-		    		str += line;
-		    	}
-		    }
-		    catch (IOException e)
-			{
-		    	System.exit(0);
-		    }
-		    
-		    MessageDialog.openInformation(shell, "Information", "Errors : "+errstr);
-		    MessageDialog.openInformation(shell, "Information", "StdOut : "+str);
-		} 
-		catch (IOException e)
-		{
-			System.err.println(e);
-			System.exit(1);			
+			System.setProperty("Directories.RootPath",
+					org.inria.mtl.MTLPlugin.getDefault().getLocation() + sep + "MTL" + sep + "bin");
+			rootpath = System.getProperty("Directories.RootPath");
 		}
-		
+		// Get the selected item in the view
+		if (selection instanceof StructuredSelection)
+		{
+			IProject currentProject = null;
+			currentSelection = (StructuredSelection)selection;
+			// We assume (FIXME) that user can only select one project
+			// disallow multi selection of projects?
+			if (currentSelection.size()==1 
+					&& currentSelection.getFirstElement() instanceof IProject)
+			{
+				currentProject = (IProject)currentSelection.getFirstElement();//.getProject?
+				project_path = currentProject.getLocation().toOSString();
+			}
+		}
 
+		// Get the necessary variable to execute the external antimtl python command
+		String cmd_path = "python/src/org/irisa/antimtl/Antimtl.py";
+		String templates_path = "python/templates";
+		String options   = " --output="+project_path;	
+		options 		+= " --templates_path="+this.getPathFromRelative(templates_path);
 		
+		// Try to execute the python external command, if a project was selected
+		if (project_path!="")
+		{
+			Process py_proc;			// Here we ask the user to give the path of BasicMTLc.jar
+			try
+			{
+				py_proc = Runtime.getRuntime().exec("python "
+						+this.getPathFromRelative(cmd_path)
+						+options);
+				InputDialog idialog = new InputDialog(shell, "get BasicMTLc.jar path", "Path of the _directory_ of BasicMTLc.jar...", 
+						rootpath,null);
+				
+				int code = idialog.open();
+				// User can cancell her action
+				if (code != InputDialog.CANCEL)
+				{
+					String i_basicmtlpath = idialog.getValue();
+					options			+= " --jardir="+i_basicmtlpath;
+					// Wheres we display the output of the python command 
+					BufferedReader error_stream = new BufferedReader(
+							new InputStreamReader(py_proc.getErrorStream()));
+					BufferedReader input_stream = new BufferedReader(
+							new InputStreamReader(py_proc.getInputStream()));
+					while ((line = error_stream.readLine()) != null) 
+						errstr += line;
+					if (errstr!="")
+						MessageDialog.openInformation(shell, "Information", "Errors : " + errstr);
+				} 
+			}
+			catch (IOException e1)
+			{
+				System.err.println("Error during the execution of Antimtl.py command");
+				e1.printStackTrace();
+			}
+		}
+		else // if none selected, or too many selections, or selection is not a Project
+		{
+			MessageDialog.openInformation(shell, "Oups", "No selected project (or too many selections?)");
+		}
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection)
