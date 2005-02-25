16cf1d5a38f72f515cd14d74f98db18fc37276bb
Didier Vojtisek <didier.vojtisek@inria.fr>
2005-02-25 10:21:31 +0000

    initial release
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mtl/trunk@1007 da3e8b17-bafa-471d-a888-b3674b21d29d

---
+++ RepositoryDriver_projects/MDRDriver/src/java/org/irisa/triskell/MT/repository/MDRDriver/Java/ImplementedModel.java
@@ -0,0 +1,27 @@
+/*
+ * $Id: ImplementedModel.java,v 1.1 2005-02-25 10:21:31 dvojtise Exp $
+ * Authors : dvojtise
+ * 
+ * Copyright 2004 - INRIA - LGPL license
+ */
+package org.irisa.triskell.MT.repository.MDRDriver.Java;
+
+
+public class ImplementedModel 
+    extends org.irisa.triskell.MT.repository.genericJMIDriver.Model
+{
+	
+    public void load(
+        org.irisa.triskell.MT.repository.genericJMIDriver.JMIAPI api)
+        throws java.lang.Exception
+    {
+    	// Do nothing
+    }
+
+    public void store(
+        org.irisa.triskell.MT.repository.genericJMIDriver.JMIAPI api)
+        throws java.lang.Exception
+    {
+    	// Do nothing
+    }
+}
